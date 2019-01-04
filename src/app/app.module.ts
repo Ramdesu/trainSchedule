45bd960f507fc5ca45c92f1b54dba8d048ee15f9
Ramdesu <ramdesu@yandex.ru>
2019-01-04 15:19:39 +0300

    route settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import { ScheduleComponent } from './schedule/schedule.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes =[
+  { path: '', component: HomeComponent},
+  { path: 'timetable', component: ScheduleComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ScheduleComponent
+    ScheduleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
