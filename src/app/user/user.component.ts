dc0374141ac8d6e1f02267605c7dbf08721664a4
Ramdesu <ramdesu@yandex.ru>
2019-01-04 18:06:30 +0300

    created user control panel

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+interface IForm {
+  name: string;
+  surname: string;
+  age: number;
+  city: string;
+}
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent {
+  name: string = localStorage.getItem('name');
+  surname: string = localStorage.getItem('surname');
+  age: string = localStorage.getItem('age');
+  city: string = localStorage.getItem('city');
+
+  addUser(): void {
+    localStorage.setItem('name', this.name);
+    localStorage.setItem('surname', this.surname);
+    localStorage.setItem('age', this.age);
+    localStorage.setItem('city', this.city);
+  }
+}
